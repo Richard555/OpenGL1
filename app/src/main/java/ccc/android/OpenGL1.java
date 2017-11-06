c4e560af9ec64bc02ebea28a0d38bd3ae21c1b04
RichardChen <richardchen@via.com.tw>
2017-11-06 18:33:21 +0800

    Upgrade to GLES2.0

--- app/src/main/java/ccc/android/OpenGL1.java
+++ app/src/main/java/ccc/android/OpenGL1.java
@@ -1,6 +1,7 @@
 package ccc.android;
 
 import android.app.Activity;
+import android.opengl.GLES20;
 import android.os.Bundle;
 import android.content.Context;  
 import android.opengl.GLSurfaceView;
@@ -23,8 +24,11 @@ class GlView extends GLSurfaceView{
 	  private GlRender mGlRender;   
 	  
 	  public GlView(Context context){  
-	    super(context);  
-	      
+	    super(context);
+
+	  // Create an OpenGL ES 2.0 context
+	  setEGLContextClientVersion(2);
+
 	    mGlRender = new GlRender();  
 	    setRenderer(mGlRender);  
 	  }  
@@ -34,23 +38,30 @@ class GlRender implements GLSurfaceView.Renderer{
     
 	  @Override  
 	  public void onSurfaceCreated(GL10 gl,EGLConfig config){  
-	    gl.glClearColor( 0.0f, 1.0f, 0.0f, 1.0f);// 3D背景色rgba
-	  }  
+//	    gl.glClearColor( 0.2f, 1.0f, 0.2f, 1.0f);// 3D背景色rgba
+        GLES20.glClearColor(0.2f, 0.0f, 0.0f, 1.0f);
+	  }
 	  
 	  @Override  
 	  public void onSurfaceChanged(GL10 gl,int w,int h){  
-	    gl.glViewport( 0, 0, w, h );  
-	    gl.glMatrixMode( GL10.GL_PROJECTION);  
-	    gl.glLoadIdentity();
-	    float ratio;  
-	    ratio = (float)w/h;  
-	    gl.glFrustumf(-ratio,ratio,-1,1,1,500);  
-	  }  
+//	    gl.glViewport( 0, 0, w, h );
+//	    gl.glMatrixMode( GL10.GL_PROJECTION);
+//	    gl.glLoadIdentity();
+//	    float ratio;
+//	    ratio = (float)w/h;
+//	    gl.glFrustumf(-ratio,ratio,-1,1,1,500);
+		int width = w;
+		int height =h;
+
+		GLES20.glViewport(0, 0, width, height);
+	  }
 	  
 	  @Override  
 	  public void onDrawFrame(GL10 gl){  
-	    gl.glClear(GL10.GL_COLOR_BUFFER_BIT);  
-	    gl.glMatrixMode(GL10.GL_MODELVIEW);  
-	    gl.glLoadIdentity();  
+//	    gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+//	    gl.glMatrixMode(GL10.GL_MODELVIEW);
+//	    gl.glLoadIdentity();
+		// Redraw background color
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 	  }  
 }  
